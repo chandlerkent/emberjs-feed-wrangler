f2d7ad776d33375ac2661406c151f3da7fe588e2
Chandler Kent <chandler.kent@gmail.com>
2013-08-10 17:08:34 +0000

    Redirects the user from the login route to the index route if they are already logged in.

--- client/app/routes/login_route.js
+++ client/app/routes/login_route.js
@@ -1,4 +1,10 @@
 App.LoginRoute = Ember.Route.extend({
+  redirect: function() {
+    if (App.API.get("isAuthenticated")) {
+      this.transitionTo("index"); 
+    }
+  },
+  
   setupController: function(controller) {
     controller.reset(); 
   }
