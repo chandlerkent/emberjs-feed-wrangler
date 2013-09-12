c9635bda4364890949f2f27b05fc3a9ed93190fd
Chandler Kent <chandler.kent@gmail.com>
2013-09-12 20:36:20 +0000

    Removes unused dependencies.

--- Gruntfile.js
+++ Gruntfile.js
@@ -110,7 +110,6 @@ module.exports = function(grunt) {
   
   // grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-neuter');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-ember-templates');
